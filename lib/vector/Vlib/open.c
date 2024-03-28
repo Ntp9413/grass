fd52c6e50dbc1c57e082b2a71f92786e7768f931
Ntp9413 <156957724+Ntp9413@users.noreply.github.com>
2024-03-28 11:33:28 -0400

    Update lib/vector/Vlib/open.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/vector/Vlib/open.c
+++ lib/vector/Vlib/open.c
@@ -555,8 +555,7 @@ int Vect__open_old(struct Map_info *Map, const char *name, const char *mapset,
     if (update && !head_only) {
         char file_path[GPATH_MAX];
         Vect__get_element_path(file_path, Map, GV_TOPO_ELEMENT);
-        unlink(file_path); \
-
+        unlink(file_path);
 
         Vect__get_element_path(file_path, Map, GV_SIDX_ELEMENT);
         unlink(file_path); 
